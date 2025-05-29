72fe470f335e71a4e8f929d33e11ea1a2b8e1dbc
aacebo <aacebowork@gmail.com>
2025-05-29 10:31:33 -0400

    Create div.go

---
+++ html/div.go
@@ -0,0 +1,54 @@
+package html
+
+import (
+	"github.com/thegogod/mde/core"
+)
+
+type DivElement struct {
+	element *Element
+}
+
+func Div() *DivElement {
+	return &DivElement{Elem("div")}
+}
+
+func (self *DivElement) Id(value string) *DivElement {
+	self.element.Id(value)
+	return self
+}
+
+func (self *DivElement) Style(styles ...core.KeyValue[string, string]) *DivElement {
+	self.element.Style(styles...)
+	return self
+}
+
+func (self *DivElement) Class(classes ...string) *DivElement {
+	self.element.Class(classes...)
+	return self
+}
+
+func (self *DivElement) Attr(name string, value string) *DivElement {
+	self.element.Attr(name, value)
+	return self
+}
+
+func (self *DivElement) Add(children ...any) *DivElement {
+	self.element.Add(children...)
+	return self
+}
+
+func (self DivElement) String() string {
+	return self.element.String()
+}
+
+func (self DivElement) PrettyString(indent string) string {
+	return self.element.PrettyString(indent)
+}
+
+func (self DivElement) Bytes() []byte {
+	return []byte(self.String())
+}
+
+func (self DivElement) PrettyBytes(indent string) []byte {
+	return []byte(self.PrettyBytes(indent))
+}
