55bcd66ed5eb1b6cc146f7995d4ed3db4b4d00cc
aacebo <aacebowork@gmail.com>
2025-06-03 13:18:49 -0400

    Update parser.go

--- core/parser.go
+++ core/parser.go
@@ -5,6 +5,4 @@ type Parser interface {
 	Parse(src []byte) (Node, error)
 	ParseBlock(iter Iterator) (Node, error)
 	ParseInline(iter Iterator) (Node, error)
-	ParseText(iter Iterator) ([]byte, error)
-	ParseTextUntil(iter Iterator, kind byte) ([]byte, error)
 }
